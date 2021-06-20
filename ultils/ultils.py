4ca1eff2448e14f67d6186b44e05f06c2c4eaf94
Philip <phuong.taidai@ascentis.com.sg>
2021-06-20 17:30:54 +0700

    add Ultils func

---
+++ ultils/ultils.py
@@ -0,0 +1,15 @@
+import shutil
+import datetime
+import os
+from google.colab import drive
+
+def movefile(pathFile):
+        date = datetime.datetime.now().strftime("%m%d%Y")
+        dest_file = "/content/drive/MyDrive/BillardAI/Models/best_" + date + ".pt"
+        exist = os.path.isfile(dest_file)
+
+        if exist:
+            os.remove(dest_file)
+            print("file has removed : " + "best_" + date + ".pt")
+            
+        shutil.move(pathFile, dest_file)
